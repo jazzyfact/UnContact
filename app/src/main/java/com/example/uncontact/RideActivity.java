28a1051483971fae063f912ba45f5f8802f0dd92
jazzyfact <jazzyfact2000@gmail.com>
2020-06-29 21:29:38 +0900

    [add]MainActivity에서 TravelActivity이동 [add]드슈,사슈,자슈,타슈 Activity 생성

---
+++ app/src/main/java/com/example/uncontact/RideActivity.java
@@ -0,0 +1,14 @@
+package com.example.uncontact;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ride);
+    }
+}
