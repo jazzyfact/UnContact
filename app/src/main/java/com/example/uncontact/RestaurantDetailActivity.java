fbc9ae68a647fd4172a176ed52e47db8fccd3205
jazzyfact <jazzyfact2000@gmail.com>
2020-06-30 15:14:46 +0900

    [add]RestaurantDetailActivity [add]restaurantSelectMenuActivity add

---
+++ app/src/main/java/com/example/uncontact/RestaurantDetailActivity.java
@@ -0,0 +1,15 @@
+package com.example.uncontact;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+//드슈 식당 상세정보 페이지
+public class RestaurantDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_detail);
+    }
+}
